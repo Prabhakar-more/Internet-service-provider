aa3bc5d9f5322c8bb47a5c0141efef66511ba20c
prabh <prabh@192.168.1.33>
2025-08-13 00:42:31 +0530

    initial commit

--- isp-swing/src/main/java/com/isp/app/Main.java
+++ isp-swing/src/main/java/com/isp/app/Main.java
@@ -1,9 +1,30 @@
+//package com.isp.app;
+////package com.isp.app.*;
+//import javax.swing.SwingUtilities;
+//
+//import com.formdev.flatlaf.FlatLightLaf;
+//import com.isp.app.view.AdminFrame;
+////this is Main class
+//public class Main {
+//    public static void main(String[] args) {
+//        // Setup modern look & feel
+//        FlatLightLaf.install();
+//
+//        SwingUtilities.invokeLater(() -> {
+//            AdminFrame frame = new AdminFrame();
+//            frame.setVisible(true);
+//        });
+//    }
+//}
+
+
 package com.isp.app;
-import javax.swing.SwingUtilities;
 
 import com.formdev.flatlaf.FlatLightLaf;
-//import com.isp.app.view.AdminFrame;
-//this is Main class
+import com.isp.app.view.*;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         // Setup modern look & feel
@@ -15,5 +36,3 @@ public class Main {
         });
     }
 }
-
-
