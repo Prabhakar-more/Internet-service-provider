eca8aa3c304c7d3433d97137aec009c45e922c8b
prabh <prabh@192.168.1.33>
2025-08-13 00:20:10 +0530

    initial comment 3

--- isp-swing/src/main/java/com/isp/app/Main.java
+++ isp-swing/src/main/java/com/isp/app/Main.java
@@ -1,9 +1,8 @@
 package com.isp.app;
-import com.isp.app.*;
 import javax.swing.SwingUtilities;
 
 import com.formdev.flatlaf.FlatLightLaf;
-import com.isp.app.view.AdminFrame;
+//import com.isp.app.view.AdminFrame;
 //this is Main class
 public class Main {
     public static void main(String[] args) {
